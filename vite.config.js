b16af00570b3c06018bc3153928a97deec7f9de9
Saxum-0 <165333677+Saxum-0@users.noreply.github.com>
2025-05-12 17:28:26 +0200

    Remove vite-static-copy from config

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   base: '/',
   plugins: [
     vue(),
     vueDevTools(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.' // vers dist/
-        }
-      ]
-    })
   ],
   resolve: {
     alias: {
@@ -24,3 +15,4 @@ export default defineConfig({
     },
   },
 })
+
