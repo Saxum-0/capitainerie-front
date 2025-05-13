886e4863bf49946f6bd655577a53d17714f31849
Saxum-0 <165333677+Saxum-0@users.noreply.github.com>
2025-05-13 19:27:34 +0200

    Fix

---
+++ src/views/ToastTest.vue
@@ -0,0 +1,16 @@
+<!-- src/components/ToastTest.vue -->
+<template>
+  <div>
+    <button @click="testToast">Test Toast</button>
+  </div>
+</template>
+
+<script setup>
+import { useToast } from 'vue-toastification';
+
+const toast = useToast();
+
+const testToast = () => {
+  toast.success("✅ Ça fonctionne !");
+};
+</script>
