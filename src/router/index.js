beaeb10b2dacefc0bf0f6649a61b1b3e234a76d8
Saxum-0 <165333677+Saxum-0@users.noreply.github.com>
2025-05-13 10:50:55 +0200

    Fix

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard, meta: { requiresAuth: true } },
   { path: '/catways', name: 'Catways', component: Catways, meta: { requiresAuth: true } },
-  { path: '/catways/:id', name: 'CatwayDetails', component: CatwayDetails, meta: { requiresAuth: true } },
+  { path: '/catway/:id', name: 'CatwayDetails', component: CatwayDetails, meta: { requiresAuth: true } },
   { path: '/reservations', name: 'Reservations', component: Reservations, meta: { requiresAuth: true } },
   { path: '/reservation/:id', name: 'ReservationDetails', component: ReservationDetails, meta: { requiresAuth: true } },
   { path: '/documentation', name: 'Documentation', component: Documentation },
